3b568bbffa2453fad21046fbee91088b4c32e610
JS <rolland.js@gmail.com>
2019-03-11 23:44:25 +0100

    this props in class component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>React App</h1>
+        <h1>{this.props.titre}</h1>
+
         <Membre nom="Antho" />
         <Membre nom="Ségolène" />
         <Membre nom="Eléanor" />
