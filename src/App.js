8710ef9203cf3445b682e0dd815739ab60594567
JS <rolland.js@gmail.com>
2019-03-11 23:54:35 +0100

    Création d'un state 'famille'

--- src/App.js
+++ src/App.js
@@ -2,18 +2,42 @@ import React, { Component } from 'react'
 import './App.css'
 import Membre from './components/Membre'
 
+const famille = {
+  membre1: {
+    nom: 'Antho',
+    age: 27,
+  },
+  membre2: {
+    nom: 'Ségolène',
+    age: 27,
+  },
+  membre3: {
+    nom: 'Eléanor',
+    age: 0,
+  },
+  membre4: {
+    nom: 'Mocha',
+    age: 2
+  }
+}
+
 class App extends Component {
+  state = {
+    famille,
+  }
+
   render() {
     const { titre } = this.props
+    const { famille } = this.state
+
     return (
       <div className="App">
         <h1>{titre}</h1>
 
-        <Membre nom="Antho" />
-        <Membre nom="Ségolène" />
-        <Membre nom="Eléanor" />
-
-        <Membre nom="Mocha">
+        <Membre nom={famille.membre1.nom} />
+        <Membre nom={famille.membre2.nom} />
+        <Membre nom={famille.membre3.nom} />
+        <Membre nom={famille.membre4.nom}>
           <strong>Je suis un chien</strong>
         </Membre>
       </div>
