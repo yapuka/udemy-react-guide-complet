78416aa77e5a1fc75ce4aa5196b566a2e40d3aa7
JS <rolland.js@gmail.com>
2019-03-11 23:42:59 +0100

    Props as object in component stateless

--- src/components/Membre.js
+++ src/components/Membre.js
@@ -1,8 +1,7 @@
 import React from 'react'
 
-const Member = (props) => {
-  const name = props.nom;
-  return <h2>Membre de ma famille : {name.toUpperCase()}</h2>
+const Member = ({ nom }) => {
+  return <h2>Membre de ma famille : {nom.toUpperCase()}</h2>
 }
 
 export default Member
